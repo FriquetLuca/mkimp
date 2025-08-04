d0e8b5a1d0a47b229b039bc9ad0db422849eccbd
Luca F. <friquet.luca@gmail.com>
2025-08-05 01:36:29 +0200

    add test for include

---
+++ examples/foo.md
@@ -0,0 +1,16 @@
+# Foo
+
+foo!
+
+!INCLUDE "./bar.md"
+!INCLUDE "./foo.md"
+
+After the imports... [^import]
+
+[^import]: You should know what an import is!
+
+We should have a foo note.
+
+Don't you think so ?
+
+This *is real
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,8 +1,42 @@
 import { MkImp } from "../src"
+import path from "path"
+import fs from "fs"
 
 test("frontmatter should be found correctly", async () => {
     const mkimp = await new MkImp().ast(
         '----\n{\n"hello":"world"\n}\n----\nHello!'
     )
     expect(mkimp.metadata).toEqual(new Map([["hello", "world"]]))
+})
+
+test("include", async () => {
+    const mkimp = new MkImp({
+        async include(location, from, to) {
+            const fullPath = path.resolve(__dirname, "../examples", location)
+            const fileContent = fs.readFileSync(fullPath, "utf-8")
+            if (from === undefined && to === undefined) {
+                return fileContent
+            }
+            const filelines = fileContent.split(/\r?\n/)
+            const newFrom = Math.max(
+                0,
+                Math.min(from ? from - 1 : 0, filelines.length - 1)
+            )
+            const newTo = Math.max(
+                0,
+                Math.min(
+                    to ? to - 1 : filelines.length - 1,
+                    filelines.length - 1
+                )
+            )
+            return filelines.slice(newFrom, newTo).join("\n")
+        },
+    })
+    const location = path.resolve(__dirname, "../examples/foo.md")
+    console.log(location)
+    const fileContent = fs.readFileSync(location, "utf-8")
+    const ast = await mkimp.ast(fileContent)
+    const result = mkimp.render(ast)
+    console.log(result)
+    expect(result.length).toBeGreaterThan(0)
 })
